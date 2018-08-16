5c16b4c775f50112370c76be0e82cdf77210221e
elaubinger <elaubinger@yahoo.com>
2018-08-16 15:28:28 -0400

    Added sample usage project

---
+++ ActionRetry/SampleUsage/Program.cs
@@ -0,0 +1,18 @@
+﻿using ActionRetry;
+
+namespace SampleUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var retry = new Retry(() =>
+            {
+                // Action to retry, returns true when completed successfully
+                return true;
+            });
+
+            while (!retry.Begin()) { }
+        }
+    }
+}
