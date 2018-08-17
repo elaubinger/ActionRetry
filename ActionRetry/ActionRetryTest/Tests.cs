386cb3c212c09a03724d605d71f6049a3ca2df12
elaubinger <elaubinger@yahoo.com>
2018-08-16 22:18:24 -0400

    more tests, mostly covering exception handling

--- ActionRetry/ActionRetryTest/Tests.cs
+++ ActionRetry/ActionRetryTest/Tests.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ActionRetry;
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace ActionRetryTest
 {
@@ -26,6 +27,100 @@ namespace ActionRetryTest
                     await Task.Delay(0);
                     return false;
                 }, backoff: enumVal).BeginASync());
+            }
+        }
+
+        [TestMethod]
+        public async Task Exceptions_Unhandled()
+        {
+            foreach (Retry.Backoff enumVal in Enum.GetValues(typeof(Retry.Backoff)))
+            {
+                try
+                {
+                    new Retry(toRetry: () => throw new Exception(), backoff: enumVal).Begin();
+                    Assert.Fail();
+                }
+                catch (Exception) { }
+
+                try
+                {
+                    await new Retry(async () =>
+                    {
+                        await Task.Delay(0);
+                        throw new Exception();
+                    }, backoff: enumVal).BeginASync();
+                    Assert.Fail();
+                }
+                catch (Exception) { }
+            }
+        }
+
+        [TestMethod]
+        public async Task Exceptions_Ignored()
+        {
+            foreach (Retry.Backoff enumVal in Enum.GetValues(typeof(Retry.Backoff)))
+            {
+                try
+                {
+                    new Retry(toRetry: () => throw new Exception(), ignoreExceptions: true,  backoff: enumVal).Begin();
+                }
+                catch (Exception) { Assert.Fail(); }
+
+                try
+                {
+                    await new Retry(async () =>
+                    {
+                        await Task.Delay(0);
+                        throw new Exception();
+                    }, ignoreExceptions: true, backoff: enumVal).BeginASync();
+                }
+                catch (Exception) { Assert.Fail(); }
+            }
+        }
+
+        [TestMethod]
+        public async Task Exceptions_Blacklist()
+        {
+            foreach (Retry.Backoff enumVal in Enum.GetValues(typeof(Retry.Backoff)))
+            {
+                try
+                {
+                    new Retry(toRetry: () => throw new ArgumentException(), exceptionBlacklist: new HashSet<Type>() { typeof(ArgumentException) }, backoff: enumVal).Begin();
+                }
+                catch (ArgumentException) { }
+
+                try
+                {
+                    await new Retry(async () =>
+                    {
+                        await Task.Delay(0);
+                        throw new ArgumentException();
+                    }, exceptionBlacklist: new HashSet<Type>() { typeof(ArgumentException) }, backoff: enumVal).BeginASync();
+                }
+                catch (ArgumentException) { }
+            }
+        }
+
+        [TestMethod]
+        public async Task Exceptions_Whitelist()
+        {
+            foreach (Retry.Backoff enumVal in Enum.GetValues(typeof(Retry.Backoff)))
+            {
+                try
+                {
+                    new Retry(toRetry: () => throw new ArgumentException(), exceptionWhitelist: new HashSet<Type>() { typeof(ArgumentException) }, backoff: enumVal).Begin();
+                }
+                catch (ArgumentException) { Assert.Fail(); }
+
+                try
+                {
+                    await new Retry(async () =>
+                    {
+                        await Task.Delay(0);
+                        throw new ArgumentException();
+                    }, exceptionWhitelist: new HashSet<Type>() { typeof(ArgumentException) }, backoff: enumVal).BeginASync();
+                }
+                catch (ArgumentException) { Assert.Fail(); }
             }
         }
     }
